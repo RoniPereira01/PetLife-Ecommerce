6de325a27d6e80f73de4b0f3adf0ef530ada8fc8
Ronilson Pereira <ronilsonpereira.pro@gmail.com>
2024-07-16 18:35:22 -0300

    mudança do navbar

--- assets/js/global/scriptPadrao.js
+++ assets/js/global/scriptPadrao.js
@@ -1,14 +1,34 @@
-//nesse script ele faz a funcao quando o botao for clicado ele aparece a barra menu
-//na tela.
+
 let show = true;
+
 const menuContent = document.querySelector('.content');
 const menuToggle = menuContent.querySelector('.menu-toggle');
 
-
 menuToggle.addEventListener('click', () => {
-
-    document.body.style.overflow = show ? 'hidden' : 'initial'
+    if (show) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'initial';
+    }
 
     menuContent.classList.toggle('on', show);
     show = !show;
-})
+});
+
+const searchInput = document.querySelector("#searchInput");
+const cards = document.querySelectorAll(".card");
+
+const filterData = () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    cards.forEach(card => {
+        const title = card.querySelector("h3").textContent.toLowerCase();
+        const description = card.querySelector("p").textContent.toLowerCase();
+        if (title.includes(searchTerm) || description.includes(searchTerm)) {
+            card.style.display = "";
+        } else {
+            card.style.display = "none";
+        }
+    });
+};
+
+searchInput.addEventListener("input", filterData);
